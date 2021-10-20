c4956d934372b8ff5113eff396eb3267b0df0122
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-21 01:18:14 +0900

    update ALDS1_13_C

--- ALDS1_13/ALDS1_13_C.cpp
+++ ALDS1_13/ALDS1_13_C.cpp
@@ -1,95 +1,86 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int board[4][4];
-int ans=45;
-const int dy[]={1,0,-1,0};
-const int dx[]={0,1,0,-1};
-
-// 位置を値に変換
-int to_v(int y,int x){
-  return y*4+x+1;
-}
-
-bool is_correct(){
-  for(int i=0;i<4;i++){
-    for(int j=0;j<4;j++){
-      // 右下は除く
-      if(i==3&&j==3) continue;
-      if(board[i][j]!=to_v(i,j)){
-        return false;
-      }
+vector<vector<int>> input() {
+  vector<vector<int>> board(4, vector<int>(4));
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      cin >> board[i][j];
     }
   }
-  return true;
+  return board;
 }
 
-// マンハッタン距離
-int manhattan(){
-  int res=0;
-  for(int i=0;i<4;i++){
-    for(int j=0;j<4;j++){
-      if(board[i][j]==0){
-        continue;
-      }
-      int v=board[i][j]-1;
-      res+=abs(v/4-i)+abs(v%4-j);
+pair<int, int> start_pos(const vector<vector<int>> &board) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (board[i][j] == 0) { return pair(i, j); }
     }
   }
-  return res;
+  return pair(-1, -1);
 }
 
-bool range_check(int y,int x){
-  return 0<=y&&y<4&&0<=x&&x<4;
-}
-
-void debug_output(){
-  cerr<<"-------------------\n";
-  for(int i=0;i<4;i++){
-    for(int j=0;j<4;j++){
-      cerr<<board[i][j]<<' ';
-    }
-    cerr<<endl;
-  }
-}
-
-// depth, ヒューリスティック値, 前回動いた方向, y, x
-void dfs(int d,int h,int prev,int y,int x){
-  if(is_correct()){
-    ans=min(ans,d);
-    return;
-  }
-  if(d+h>ans) return;
-
-  for(int dir=0;dir<4;dir++){ 
-    int ny=y+dy[dir];
-    int nx=x+dx[dir];
-
-    // 範囲外
-    if(range_check(ny,nx)==false) continue;
-    // 行ったり来たりしない
-    if(abs(prev-dir)==2) continue;
-
-    swap(board[y][x],board[ny][nx]);
-    dfs(d+1,manhattan(),dir,ny,nx);
-    swap(board[y][x],board[ny][nx]);
-  }
-}
-
-int main(){
-  int sy,sx;
-  for(int i=0;i<4;i++){
-    for(int j=0;j<4;j++){
-      cin>>board[i][j];
-      if(board[i][j]==0){
-        sy=i;
-        sx=j;
-      }
+vector<vector<int>> correct() {
+  vector<vector<int>> board(4, vector<int>(4));
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      board[i][j] = 4 * i + (j + 1);
     }
   }
-  const int INF = 1001001001;
-  dfs(0,manhattan(),INF,sy,sx);
-  cout<<ans<<endl;
-  return 0;
+  board[3][3] = 0;
+  return board;
+}
+
+int manhattan(const vector<vector<int>> &board) {
+  int sum = 0;
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      if (board[i][j] == 0) continue;
+      int v = board[i][j] - 1;
+      sum += abs(i - v / 4) + abs(j - v % 4);
+    }
+  }
+  return sum;
+}
+
+int main() {
+  vector<vector<int>> board = input();
+
+  auto [sy, sx] = start_pos(board);
+
+  vector<vector<int>> cor = correct();
+
+  constexpr int dy[] = { 0, 1, 0, -1 };
+  constexpr int dx[] = { 1, 0, -1, 0 };
+
+  int ans = 45;
+
+  auto dfs = [&](auto &&f, int y, int x, int dep, int dist, int pre) -> void {
+    if (board == cor) {
+      ans = min(ans, dep);
+      return;
+    }
+
+    if (dep + dist > ans) return;
+
+    for (int i = 0; i < 4; i++) {
+      if (abs(i - pre) == 2) continue;
+
+      int ny = y + dy[i];
+      int nx = x + dx[i];
+
+      if (ny < 0 or ny >= 4 or nx < 0 or nx >= 4) continue;
+
+      swap(board[y][x], board[ny][nx]);
+
+      f(f, ny, nx, dep + 1, manhattan(board), i);
+
+      swap(board[y][x], board[ny][nx]);
+    }
+  };
+
+  dfs(dfs, sy, sx, 0, manhattan(board), -10101);
+
+  cout << ans << endl;
 }
