c74faf729b13eb5a5a624bcd02d8f93b8f8a1b58
matumoto1234 <matumotosizuoka@gmail.com>
2021-11-15 12:12:07 +0900

    Update ALDS1_7

--- ALDS1_7/ALDS1_7_A.cpp
+++ ALDS1_7/ALDS1_7_A.cpp
@@ -1,110 +1,95 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct Node{
-  int p;
-  int depth;
-  vector<int> Childlen;
-};
+int n;
+vector<int> ids, parents, depths;
+vector<string> types;
+vector<vector<int>> children;
+vector<string> answers;
 
-// Tをグローバル変数にしても良い
-// vector<Node>& TはC言語でいうところのアドレス渡し
-// これでvectorを渡してる
-void getDepth(int index,int depth,vector<Node>& T){
-  T[index].depth=depth;
-
-  int sz=T[index].Childlen.size();
-  // 子がないなら終了
-  if(sz==0){
-    return;
+ostream &operator<<(ostream &os, const vector<int> &vs) {
+  for (size_t i = 0; i < vs.size(); i++) {
+    if (i) os << ", ";
+    os << vs[i];
   }
-
-  for(int j=0;j<sz;j++){
-    // 子に向かうので深さを+1
-    int to=T[index].Childlen[j];
-    getDepth(to,depth+1,T);
-  }
-
-  return;
+  return os;
 }
 
-// Tをグローバル変数にしても良い
-// vector<Node>& Tは長い
-// T[i].Childlen[j]を出力する
-void outputChildlen(int index,vector<Node>& T){
-  int sz=T[index].Childlen.size();
-  for(int j=0;j<sz;j++){
-    // 最初以外は", "を出力する
-    if(j>=1){
-      cout<<", ";
-    }
-    cout<<T[index].Childlen[j];
+int get_root(int id) {
+  if (parents[id] == -1) return id;
+  return get_root(parents[id]);
+}
+
+void dfs(int id, int dep) {
+  depths[id] = dep;
+
+  for (auto child: children[id]) {
+    parents[child] = id;
+    dfs(child, dep + 1);
   }
 }
 
-// Tをグローバル変数にしても良い
-// vector<Node>& Tは長い
-// 親がないなら根,子がないなら葉,それ以外は節
-string name(int index,vector<Node>& T){
-  if(T[index].p==-1) return "root";
-  else if(T[index].Childlen.size()==0) return "leaf";
-  else return "internal node";
+void set_types() {
+  for (int i = 0; i < n; i++) {
+    if (parents[i] == -1) {
+      types[i] = "root";
+      continue;
+    }
+
+    if (children[i].empty()) {
+      types[i] = "leaf";
+      continue;
+    }
+
+    types[i] = "internal node";
+  }
 }
 
-int main(){
-  int n;
-  cin>>n;
+void set_node_info(int i) {
+  int id = ids[i];
+  stringstream ss;
+  ss << "node " << id << ": ";
+  ss << "parent = " << parents[id] << ", ";
+  ss << "depth = " << depths[id] << ", ";
+  ss << types[id] << ", ";
+  ss << "[" << children[id] << "]";
+  answers[id] = ss.str();
+}
 
-  // Graph
-  vector<vector<int>> G(n);
-  for(int i=0;i<n;i++){
-    int id,k;
-    cin>>id>>k;
-    for(int j=0;j<k;j++){
-      int child;
-      cin>>child;
-      G[id].push_back(child);
+int main() {
+  cin >> n;
+
+  ids.resize(n);
+  parents.assign(n, -1);
+  depths.resize(n);
+  types.resize(n);
+  children.resize(n);
+  answers.resize(n);
+
+  for (int i = 0; i < n; i++) {
+    int id, k;
+    cin >> id >> k;
+
+    ids[i] = id;
+
+    vector<int> cs(k);
+    for (int &c: cs) {
+      cin >> c;
+      parents[c] = id;
     }
+
+    children[id] = cs;
   }
 
-  // Treeの初期化
-  vector<Node> T(n);
-  for(int i=0;i<n;i++){
-    T[i].p=-1;
+  dfs(get_root(0), 0);
+
+  set_types();
+
+  for (int i = 0; i < n; i++) {
+    set_node_info(i);
   }
 
-  // 親を決める
-  for(int i=0;i<n;i++){
-    // 伸びてる辺の数だけループする
-    int sz=G[i].size();
-    for(int j=0;j<sz;j++){
-      int child=G[i][j];
-      T[child].p=i;
-      T[i].Childlen.push_back(child);
-    }
+  for (int i = 0; i < n; i++) {
+    cout << answers[i] << endl;
   }
-
-  // 根を決める
-  int root;
-  for(int i=0;i<n;i++){
-    if(T[i].p==-1) root=i;
-  }
-
-  // rootから深さを0で始めていく
-  getDepth(root,0,T);
-
-  // 出力
-  for(int i=0;i<n;i++){
-    cout<<"node "<<i;
-    cout<<": parent = "<<T[i].p;
-    // 根から目的地までの深さ
-    cout<<", depth = "<<T[i].depth;
-    // 名前を出力
-    cout<<", "<<name(i,T);
-    cout<<", [";
-    // 子の出力
-    outputChildlen(i,T);
-    cout<<"]\n";
-  }
-  return 0;
 }
